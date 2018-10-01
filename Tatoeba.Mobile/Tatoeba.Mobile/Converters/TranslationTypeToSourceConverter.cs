405a225d246604b95d4db82e2a45546da884e138
Sami FERHAH <ferhah@outlook.com>
2018-10-01 23:12:55 +0200

    Replace unicode with icons (unspported by android)

---
+++ Tatoeba.Mobile/Tatoeba.Mobile/Converters/TranslationTypeToSourceConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+using Tatoeba.Mobile.Models;
+using Xamarin.Forms;
+
+namespace Tatoeba.Mobile.Converters
+{
+
+    public class TranslationTypeToSourceConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is TranslationType translationType))
+            {
+                return null;
+            }
+
+            switch (translationType)
+            {
+                case TranslationType.Original:
+                    return "original_sentence.png";
+                case TranslationType.Direct:
+                    return "direct_translation.png";
+                case TranslationType.Indirect:
+                    return "indirect_translation.png";
+                default:
+                    return "indirect_translation.png";
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
